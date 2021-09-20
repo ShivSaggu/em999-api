de8149fedc6578627a4ae730ae9a75badd270ea7
Shiv <ssaggu@deloitte.com>
2021-09-19 21:57:09 -0400

    adding accessing postgresql

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const express = require('express');
 const app = express();
 app.use(express.json());
  
+//============================ accessing postgresql ==========================//
+
+app.get('/api/dbevents', (req,res)=> {
+    res.send(events);
+}); 
+
+
+
 //===========================================================================//
 
 const events = [
